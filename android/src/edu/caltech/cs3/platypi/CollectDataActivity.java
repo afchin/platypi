1cc04a86c6b7a202e6516509fbf8c7743c79a2cb
Anton Geraschenko <geraschenko@gmail.com>
2012-04-13 17:07:38 -0700

    Added virgin android project.
    Change-Id: I75311ae8039b8050904cf59c8cbdd6fb92d482cf

---
+++ android/src/edu/caltech/cs3/platypi/CollectDataActivity.java
@@ -0,0 +1,15 @@
+package edu.caltech.cs3.platypi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CollectDataActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
